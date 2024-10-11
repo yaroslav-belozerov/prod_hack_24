290443b1bdf7ce1e9394ff79362332690c5294d5
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2024-10-11 23:47:40 +0300

    demo view of items

--- app/src/main/java/com/yaabelozerov/holodos_mobile/MainActivity.kt
+++ app/src/main/java/com/yaabelozerov/holodos_mobile/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
@@ -14,6 +17,7 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -23,6 +27,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.yaabelozerov.holodos_mobile.domain.MainScreenViewModel
 import com.yaabelozerov.holodos_mobile.ui.Navigation
 import com.yaabelozerov.holodos_mobile.ui.theme.Holodos_mobileTheme
 
@@ -30,6 +35,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val mvm: MainScreenViewModel by viewModels()
 
         setContent {
             val navController = rememberNavController()
@@ -67,7 +74,14 @@ class MainActivity : ComponentActivity() {
                             Text("Settings")
                         }
                         composable(Navigation.FRIDGE.route) {
-                            Text("Fridge")
+                            Column {
+                                mvm.items.collectAsState().value.map {
+                                    Row {
+                                        Text(it.first)
+                                        Text(it.second.toString())
+                                    }
+                                }
+                            }
                         }
                         composable(Navigation.LIST.route) {
                             Text("Shopping List")
